ae11c62e700d1fdbf5c74ac3867027fc23186085
fcaramez <francisco.caramez.duarte@gmail.com>
2022-12-16 23:45:59 +0000

    adds basic signup, creates dto's with class-validator and class-transformer

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return 'I am Signing in';
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const { email, password } = dto;
+    const hashedPassword = await argon.hash(password);
+
+    try {
+      const newUser = await this.prisma.user.create({
+        data: {
+          email,
+          hash: hashedPassword,
+        },
+        select: {
+          email: true,
+          id: true,
+        },
+      });
+
+      return {
+        ...newUser,
+        success: true,
+        message: 'Success',
+      };
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException({
+            message: 'User already exists!',
+            success: false,
+          });
+        }
+      }
+      throw err;
+    }
   }
   login() {
     return 'I am Logging in';
